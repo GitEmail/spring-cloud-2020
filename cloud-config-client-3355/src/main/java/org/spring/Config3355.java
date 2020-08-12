2c768a93a026eb4c35ccdfc5ea4885ded691d2d2
GitEmail <17722996616@189.cn>
2020-08-12 18:21:32 +0800

    修复git push丢失

--- cloud-config-client-3355/src/main/java/org/spring/Config3355.java
+++ cloud-config-client-3355/src/main/java/org/spring/Config3355.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class Config3355 {
     public static void main(String[] args) {
         SpringApplication.run(Config3355.class, args);
