ad3638b99541eeee1dd4848063d350e38a18f35c
GitEmail <17722996616@189.cn>
2020-08-12 15:58:47 +0800

    config.info

---
+++ cloud-config-client-3355/src/main/java/org/spring/controller/ConfigController.java
@@ -0,0 +1,17 @@
+package org.spring.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
