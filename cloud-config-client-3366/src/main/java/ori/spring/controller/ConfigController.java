5042107735f466df2ca34e6dff27593d7b6e8d21
GitEmail <17722996616@189.cn>
2020-08-12 22:15:22 +0800

    spring-cloud-bus整合MabbitMQ练习案例

--- cloud-config-client-3366/src/main/java/ori/spring/controller/ConfigController.java
+++ cloud-config-client-3366/src/main/java/ori/spring/controller/ConfigController.java
@@ -1,4 +1,4 @@
-package org.spring.controller;
+package ori.spring.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
